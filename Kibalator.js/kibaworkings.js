1f9f746355dfd793ddeb4d60d4f686ba4af52647
XavJad597 <92932067+XavJad597@users.noreply.github.com>
2022-10-16 09:47:30 -0300

    it doesnt work still

--- Kibalator.js/kibaworkings.js
+++ Kibalator.js/kibaworkings.js
@@ -1,54 +1,64 @@
+const powerButton = document.querySelector('[data-Power]');
+const  deleteButton = document.querySelector("[data-delete]")
+const operationButton = document.querySelectorAll("[data-operation]")
+const equalsButton = document.querySelector("[data-equal]")
+const previousNumbers = document.querySelector("[data-previous-operand]")
+const currentNumbers = document.querySelector("[data-current-operand]")
+const numberBoard = document.querySelectorAll('[data-number]')
+
+
+ 
+
+const calculator  = new Calculator (previousNumbers,currentNumbers)
+
 class Calculator {
     constructor (previousNumbers,currentNumbers){
-        this.previousNumbers = previousNumbers;
+        this.previousNumbers = previousNumbers
         this.currentNumbers = currentNumbers
         this.clear()
     }
+
+    clear () {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+ 
+    delete () {
+
+    }
+     
+  appendNumber(number){
+        this.currentOperand = number
+       
+    }
+        
+    
+    
+    
+    chooseOperation(operation){
+    
+    }
+    
+    
+    
+    compute(){
+    
+    }
+    
+    
+    
+    updateDisplay(){
+        this.currentNumbers.innerText = this.currentOperand
+    }
 }
 
-const powerButton = document.querySelector("[data-all-clear]");
-const  deleteButton = document.querySelector("[data-delete]");
-const operationButton = document.querySelectorAll("[data-number]");
-const equalsButton = document.querySelector("[data-equal]");
-const previousNumbers = document.querySelector("[data-previous-operand]");
-const currentNumbers = document.querySelector("[data-current-operand]");
-const numberBoard = document.querySelectorAll('[data-number]');
-
-clear() {
-    this.currentOperand = ''
-    this.previousOperand = ''
-    this.operation = undefined
-}
-
-const calculator = new Calculator (previousNumbers,currentNumbers);
-
-delete () {
-
-}
-
-numberBoard.forEach(button => {
- button.addEventListener("click",() =>{
+numberBoard.forEach( button => {
+    button.addEventListener('click', () =>{
         calculator.appendNumber(button.innerText)
         calculator.updateDisplay()
-      }) 
-        
-    })
-appendNumber(number){ 
-    this.currentNumbers = number
-   
-}
-    
-
-
-
-chooseOperation(operation){
-
+    } )
 }
 
-compute(){
+)
 
-}
-
-updateDisplay() {
-    this.currentNumbers.innerText = this.currentNumbers
-}
